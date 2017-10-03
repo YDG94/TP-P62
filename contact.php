d00b04b3b4d6801e4bd7a657d8caf1e1e8c509b0
Dimitri Yohann Guillaume <dguillau@isimtl.local>
2017-10-02 20:19:02 -0400

    Modif index contact css ajout images

--- contact.php
+++ contact.php
@@ -1,25 +1,35 @@
 <?php
 require_once('views/header.php');
+//var_dump($_SERVER['REQUEST_METHOD']);
+//var_dump($_POST);
+
+$input_full = array_key_exists('nom', $_POST) && array_key_exists('prenom', $_POST) && array_key_exists('courriel', $_POST);
+
 ?>
 
-<main id="contact">
-<!--    Petit formulaire de contact-->
-    <form method="get">
-        <fieldset>
-            <legend><h2>Nous Contacter</h2></legend>
-        <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="nom" placeholder="Nom">
-        <label for="prenom">Prénom :</label>
-        <input type="text" id="prenom" name="prenom" placeholder="Prénom">
-        <label for="courriel">Email :</label>
-        <input type="email" id="courriel" name="courriel" placeholder="user@yahoo.com">
-        <label for="msg">Nous laisser un commentaire :</label>
-        <textarea id="msg" name="msg" placeholder="Écrivez votre message ici" maxlength="100"></textarea>
-        </fieldset>
-        <input type="submit" value="Envoyer">
+    <main id="contact">
+        <!--    Petit formulaire de contact-->
+        <form method="post">
+            <fieldset>
+                <legend><h2>Nous Contacter</h2></legend>
+                <label for="nom">Nom :</label>
+                <input type="text" id="nom" name="nom" placeholder="Nom"
+                       value="<?= $input_full ? $_POST['nom'] : '' ?>">
+                <label for="prenom">Prénom :</label>
+                <input type="text" id="prenom" name="prenom" placeholder="Prénom"
+                       value="<?= $input_full ? $_POST['prenom'] : '' ?>">
+                <label for="courriel">Email :</label>
+                <input type="email" id="courriel" name="courriel" placeholder="user@yahoo.com"
+                       value="<?= $input_full ? $_POST['courriel'] : '' ?>">
+                <label for="msg">Nous laisser un commentaire :</label>
+                <textarea id="msg" name="msg" placeholder="Écrivez votre message ici" maxlength="100"></textarea>
+                <input type="submit" value="Envoyer">
+                <input type="reset" value="Réinitialiser">
+            </fieldset>
 
-    </form>
-</main>
+
+        </form>
+    </main>
 
 <?php
 require_once('views/footer.php');
--- index.php
+++ index.php
@@ -8,8 +8,66 @@ require_once('views/header.php');
 <main id="index">
 
 <!--    Slider des produits vedettes-->
+    <!-- Carrousel Accueil -->
+    <section id="slideshow-index">
 
-<!--    Paragraphe de présentation-->
+        <!-- Conteneur du slider au complet -->
+        <div id="container-slideshow">
+
+            <!-- Hidden input -->
+            <input checked type="radio" name="slider" id="slide1">
+            <input type="radio" name="slider" id="slide2">
+            <input type="radio" name="slider" id="slide3">
+
+            <!-- Slider -->
+            <div id="slideshow">
+
+                <!-- Début overflow -->
+                <div id="overflow">
+
+                    <!-- Conteneur des figure -->
+                    <div class="container-figure">
+
+                        <div class="figure">
+                            <img src="images/slider/bobber.jpg" alt="Photo Bobber"/>
+                            <p><strong>Villa Vue sur Mer aux Bahamas</strong>, <br><a href="">Détail de l'offre</a></p>
+                        </div>
+
+                        <div class="figure">
+                            <img src="images/slider/ninja_h2r.jpg.jpg" alt="Photo Ninja H2R"/>
+                            <p><strong>Villa Belrose en Guadeloupe</strong>, <br><a href="">Détail de l'offre</a></p>
+                        </div>
+
+                        <div class="figure">
+                            <img src="images/slider/y_z450.jpg.jpg" alt="Photo Y Z450"/>
+                            <p><strong>Villa Polimnia à Saint-Barth</strong>, <br><a href="">Détail de l'offre</a></p>
+                        </div>
+
+                    </div>
+                    <!-- Fin conteneur figure -->
+
+                </div>
+                <!-- End overflow -->
+
+            </div>
+            <!-- End Slider -->
+
+            <!-- Pagination -->
+            <div id="active">
+                <label for="slide1"></label>
+                <label for="slide2"></label>
+                <label for="slide3"></label>
+            </div>
+
+        </div>
+        <!-- Fin conteneur slider au complet -->
+
+    </section>
+    <!-- End conteneur Slider -->
+
+    <!--    Paragraphe de présentation-->
+
+
 
 </main>
 <!--FIN MAIN-->
