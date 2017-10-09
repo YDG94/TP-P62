688edba0c575a2f1fed132b85fce9b8ec86b1683
dguillau <guillaume.yohann@yahoo.com>
2017-10-09 16:03:37 -0400

    Modif Page Ninja

--- ninja_detail.php
+++ ninja_detail.php
@@ -1,10 +1,26 @@
 <?php
-$title = 'Ninja H2R 2018 | Tork Motorcycle';
+$title = 'Kawasaki Ninja H2R 2018 | Tork Motorcycle';
 require_once('views/header.php');
+require_once('defines.php');
+
+//var_dump($_GET);
+$prod_id = '';
+if (array_key_exists('prod_id', $_GET) && array_key_exists($_GET['prod_id'], $motos)) {
+    $prod_id = $_GET['prod_id'];
+}
 ?>
 
 <main id="ninja">
-
+    <?php foreach ($motos as $id => $bike) {
+        if ($prod_id == $bike) {
+            ?>
+            <h3><?= $bike[BIKE_NAME] ?></h3>
+            <img src="<?= $bike[BIKE_IMG] ?>" alt="<?= $bike[BIKE_ALT] ?>"/>
+            <h4><?= $bike[BIKE_PRICE] ?></h4>
+            <p><?= $bike[BIKE_DESC] ?></p>
+        <?php
+        }
+    } ?>
 </main>
 
 <?php
