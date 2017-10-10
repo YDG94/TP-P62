8e18b3b88c09d1146f513fa320be6f4adb3d679c
dguillau <guillaume.yohann@yahoo.com>
2017-10-10 16:41:26 -0400

    Modif page Like

--- like.php
+++ like.php
@@ -2,6 +2,22 @@
 $title = 'Favoris | Tork Motorcycle';
 require_once('views/header.php');
 require_once('defines.php');
+
+session_start();
+//$_SESSION=array();
+var_dump($_SESSION);
+
+// Création du panier s'il n'existe pas
+if (!array_key_exists('like', $_SESSION)) {
+    $_SESSION['like'] = array();
+}
+
+// Création d'une référence
+$like =& $_SESSION['like']; //$like fait référence à $_SESSION['like']
+var_dump($like); // Contenu du panier
+var_dump($_POST);
+
+
 ?>
 
 <main id="like">
