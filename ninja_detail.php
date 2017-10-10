07307f9c2b7324dd95da2869565c1a6f2e971f48
dguillau <guillaume.yohann@yahoo.com>
2017-10-10 16:41:49 -0400

    Modif page Détail

--- ninja_detail.php
+++ ninja_detail.php
@@ -5,27 +5,32 @@ require_once('defines.php');
 
 //var_dump($_GET);
 $prod_id = '';
+$bike = null;
 if (array_key_exists('prod_id', $_GET) && array_key_exists($_GET['prod_id'], $motos)) {
     $prod_id = $_GET['prod_id'];
-}else {
+    $bike=$motos[$prod_id];
+} else {
     header('Location:index.php');
     exit;
 }
+
+
 ?>
 
 <main id="ninja">
-    <?php foreach ($motos as $id => $bike) {
-        if ($prod_id == $id) { ?>
-            <button>Ajouter aux Favoris</button>
+            <form action="like.php" method="post">
+                <button type="submit" name="like" value="<?= $prod_id?>">Ajouter aux Favoris</button>
+            </form>
+
             <h3><?= $bike[BIKE_NAME] ?></h3>
             <img src="<?= $bike[BIKE_IMG] ?>" alt="<?= $bike[BIKE_ALT] ?>"/>
             <h4><?= $bike[BIKE_PRICE] ?></h4>
             <p><?= $bike[BIKE_DESC] ?></p>
-            <?php
-        }
-    } ?>
 
-    <a href="form_achat.php"><button>Procéder à l'achat</button></a>
+
+    <a href="form_achat.php">
+        <button>Procéder à l'achat</button>
+    </a>
 </main>
 
 <?php
