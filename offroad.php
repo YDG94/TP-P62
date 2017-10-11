8f167b7b7df77d87b0124c1c2705574768297c7e
dguillau <guillaume.yohann@yahoo.com>
2017-10-11 15:34:09 -0400

    Modif Offroad

--- offroad.php
+++ offroad.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+if (!array_key_exists('like', $_SESSION)) {
+    $_SESSION['like'] = array();
+}
 $title = 'Offroad | Tork Motorcycle';
 require_once('views/header.php');
 require_once('defines.php');
@@ -21,6 +25,10 @@ if (array_key_exists('cat_id', $_GET) && array_key_exists($_GET['cat_id'], $cate
                     <li>
                         <h2><?= $bike[BIKE_NAME] ?></h2>
                         <a href="<?= $bike[BIKE_URL] ?>?prod_id=<?= $id ?>">
+                            <?php if (array_key_exists($id, $_SESSION['like'])) { ?>
+                                <img src="images/boutons/like.png" alt="Icone favori"/>
+                            <?php } else { ?>
+                            <?php } ?>
                             <img src="<?= $bike[BIKE_IMG] ?>" alt="<?= $bike[BIKE_ALT] ?>"/>
                         </a>
                         <h4><?= $bike[BIKE_PRICE] ?></h4>
